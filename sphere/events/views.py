f0ead3b2f180c9cb1ddd9fc5f46fb5351f3e5af4
Владас Куодис <vlad250577@yandex.ru>
2024-08-20 12:27:13 +0300

    Add selected_related to EventListView

--- sphere/events/views.py
+++ sphere/events/views.py
@@ -18,7 +18,7 @@ class EventListView(ListView):
     template_name = 'events/index.html'
 
     def get_queryset(self):
-        return Event.objects.order_by('-pub_date')
+        return Event.objects.order_by('-pub_date').select_related('category')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
