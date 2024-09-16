6be229cdcdac26e5aafc40bfe76dae6d1c7ef30e
Владас Куодис <vlad250577@yandex.ru>
2024-09-16 13:33:23 +0300

    Add test_redirects

--- sphere/events/pytest_tests/test_routes.py
+++ sphere/events/pytest_tests/test_routes.py
@@ -31,3 +31,18 @@ def test_pages_availability_for_auth_user(client, django_user_model, name):
     client.force_login(user)
     response = client.get(url)
     assert response.status_code == HTTPStatus.OK
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    'name, kwargs',
+    [
+        ('events:event_create', None),
+        ('events:event_delete', {'slug': 'example-slug'}),
+        ('events:event_edit', {'slug': 'example-slug'}),
+    ]
+)
+def test_redirects(client, name, kwargs, event, category):
+    url = reverse(name, kwargs=kwargs)
+    response = client.get(url)
+    assert response.status_code == HTTPStatus.FOUND
